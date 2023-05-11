caa9023c3f1b7e750bbe99a830d495286aab338a
Carmen <33059@ma-web.nl>
2023-05-11 16:44:09 +0200

    video 4

--- js/main.js
+++ js/main.js
@@ -4,12 +4,23 @@ class AgendaApp {
 
     constructor() {
         this.api = new API();
+        // this.api.getData().then(result => {
+
+        // });
         this.agenda = new Agenda();
     }
 }
 
 class API {
+    constructor(){
+        this.getData();
+    }
 
+    getData(){
+        fetch("../data/data.json").then(response => {
+            console.log(response);
+        })
+    }
 }
 
 class Agenda {
@@ -20,7 +31,7 @@ class Agenda {
     constructor() {
         this.renderer = new Renderer();
         this.header = new Header();
-        this.month = new Month;
+        this.month = new Month(this);
     }
 }
 
@@ -34,20 +45,23 @@ class Header {
 
 class Month {
     days = [];
+    agenda;
 
-    constructor(){
-        for(let i = 0; i < 31; i++){
+    constructor(agenda) {
+        this.agenda = agenda;
+        for (let i = 0; i < 31; i++) {
             this.days.push(new Day(this));
         }
     }
 }
 
-class Day{
-month;
+class Day {
+    month;
 
-constructor(month){
+    constructor(month) {
+        this.month = month;
 
-}
+    }
 }
 
 const carmensAgenda = new AgendaApp();
