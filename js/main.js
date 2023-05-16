2d4242b48e59a3e5759051f23b22b95f19fece02
Carmen <33059@ma-web.nl>
2023-05-16 15:14:20 +0200

    video 10

--- js/main.js
+++ js/main.js
@@ -35,13 +35,17 @@ class Agenda {
         this.data = data;
         this.renderer = new Renderer();
         this.renderer.render("body", this.htmlElement);
-        this.header = new Header(data.name);
+        this.header = new Header(this, data.name);
         this.month = new Month(this, data.days);
+    }
+
+    render(placeToRender, whatToRender) { // Wat render je en Waar
+        this.renderer.render(placeToRender, whatToRender);
     }
 }
 
 class Renderer {
-    render(placeToRender, whatToRender){
+    render(placeToRender, whatToRender) {
         document.querySelector(placeToRender).appendChild(whatToRender);
 
     }
@@ -50,9 +54,15 @@ class Renderer {
 class Header {
     nameOfMonth;
     htmlElement;
-    constructor(nameOfMonth){
+    agenda;
+
+    constructor(agenda, nameOfMonth) {
+        this.agenda = agenda;
         this.htmlElement = document.createElement("header");
+        this.htmlElement.classList.add("agenda__header");
+        this.agenda.render(".agenda", this.htmlElement); // Class agenda "."
         this.nameOfMonth = nameOfMonth;
+        console.log(nameOfMonth);
     }
 }
 
@@ -64,21 +74,32 @@ class Month {
 
     constructor(agenda, numberOfDays) {
         this.htmlElement = document.createElement("ul");
+        this.htmlElement.classList.add("agenda__month");
         this.numberOfDays = numberOfDays;
         this.agenda = agenda;
-        for (let i = 0; i < 31; i++) {
-            this.days.push(new Day(this));
+        this.agenda.render(".agenda", this.htmlElement);
+        for (let i = 1; i < numberOfDays; i++) {
+            this.days.push(new Day(this, i));
         }
     }
-}
+
+        renderDays(placeToRender,whatToRender){
+            this.agenda.render(placeToRender, whatToRender);
+        }
+    }
+
 
 class Day {
     month;
     htmlElement;
-    constructor(month) {
-        this.htmlElement = document.createElement("li");
-        this.month = month;
+    dayNumber; 
 
+    constructor(month, dayNumber) {
+        this.dayNumber = dayNumber;
+        this.htmlElement = document.createElement("li");
+        this.htmlElement.classList.add("agenda__day");
+        this.month = month;
+        this.month.renderDays(".agenda__month", this.htmlElement);
     }
 }
 
