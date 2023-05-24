1c2ac3383aad9698637e494a5b60a8d8ebbb0248
Carmen <33059@ma-web.nl>
2023-05-24 17:28:26 +0200

    comments

--- js/agendaApp/AgendaApp.js
+++ js/agendaApp/AgendaApp.js
@@ -1,10 +1,11 @@
-class AgendaApp {
-    api;
+class AgendaApp { // blauwdruk
+    api; 
     switcher;
     month = 0;
-    constructor() {
-        this.api = new API();
-        this.switcher = new Switcher(this);
+
+    constructor() { // agenda app wordt aangemaakt
+        this.api = new API(); // nieuwe api, haalt data op
+        this.switcher = new Switcher(this); // switcher opslaan
         this.api.getData().then(result => {
             this.switcher.loadAgenda(result[this.month]);
         });
@@ -12,21 +13,22 @@ class AgendaApp {
 
     switchMonths = (sign) => {
         switch (sign) {
-            case "+":
+            case "+": // hier wordt iets opgeteld
                 this.month = this.month + 1;
                 break;
-            case "-":
+            case "-": // hier wordt iets afgehaald
                 this.month = this.month - 1;
                 break;
         }
 
-        if (this.month === 12) {
+        if (this.month === 12) { // als de maand 12 is wordt het weer januari
             this.month = 0;
         }
         if (this.month < 0) {
-            this.month = 11;
+            this.month = 11; // als de maand 11 wordt is het december
         }
 
+        // hier wordt de huidige maand geladen
         this.switcher.loadAgenda(this.api.dataFromAPI[this.month]);
     }
 }
@@ -34,6 +36,7 @@ class AgendaApp {
 class API {
     dataFromAPI = [];
 
+    // json array
     async getData() {
         await fetch("../data/data.json").then(response => {
             return response.json();
@@ -48,20 +51,27 @@ class Agenda {
     renderer;
     header;
     month;
+    data;
     htmlElement;
     agendaApp;
 
     constructor(data, agendaApp) {
+        this.data = data;
         this.agendaApp = agendaApp;
+
+        // hier wordt een article gemaakt
         this.htmlElement = document.createElement("article");
         this.htmlElement.classList.add("agenda");
-        this.data = data;
+
+        // voegt article toe aan de body
         this.renderer = new Renderer();
         this.renderer.render("body", this.htmlElement);
+        
         this.header = new Header(this, data.name, this.agendaApp);
         this.month = new Month(this, data.days);
     }
 
+    // ?
     render(placeToRender, whatToRender) {
         this.renderer.render(placeToRender, whatToRender);
     }
@@ -73,10 +83,12 @@ class Header {
     agenda;
     leftButton;
     rightButton;
+
     constructor(agenda, nameOfMonth, agendaApp) {
         this.agenda = agenda;
         this.agendaApp = agendaApp;
         this.nameOfMonth = nameOfMonth;
+
         this.htmlElement = document.createElement("header");
         this.htmlElement.classList.add("agenda__header");
         this.text = document.createElement("h2");
@@ -102,17 +114,21 @@ class Button {
     switcher;
     header;
     type;
+
     constructor(type, innerText, extraClass, header, agendaApp) {
         this.type = type;
         this.agendaApp = agendaApp;
         this.htmlElement = document.createElement("button");
         this.htmlElement.classList.add("agenda__button");
+
         this.extraClass = extraClass;
         this.htmlElement.classList.add(this.extraClass);
+
         this.innerText = innerText;
         this.htmlElement.innerText = this.innerText;
-        // this.switcher = new Switcher(this.extraClass);
+
         this.header = header;
+
         this.render();
 
         this.htmlElement.onclick = this.buttonClicked;
@@ -150,12 +166,16 @@ class Month {
     agenda;
     numberOfDays;
     htmlElement;
+    
     constructor(agenda, numberOfDays) {
         this.htmlElement = document.createElement("ul");
         this.htmlElement.classList.add("agenda__month");
+
         this.numberOfDays = numberOfDays;
         this.agenda = agenda;
+
         this.agenda.render(".agenda", this.htmlElement);
+
         for (let i = 1; i <= numberOfDays; i++) {
             this.days.push(new Day(this, i));
         }
@@ -173,11 +193,15 @@ class Day {
 
     constructor(month, dayNumber) {
         this.dayNumber = dayNumber;
+
         this.htmlElement = document.createElement("li");
         this.htmlElement.classList.add("agenda__day");
         this.htmlElement.innerText = this.dayNumber;
+
         this.month = month;
         this.month.renderDays(".agenda__month", this.htmlElement);
     }
 
 }
+
+const carmensAgenda = new AgendaApp();
